e402d040d3ffbe61ea1e2177105473169e83e41e
123 <kolliprasanthi14@gmail.com>
2024-02-26 11:33:22 +0530

    empty array impl

--- mockito-demo/src/main/java/com/example/mockito/mockitodemo/business/SampleBusinessImpl.java
+++ mockito-demo/src/main/java/com/example/mockito/mockitodemo/business/SampleBusinessImpl.java
@@ -4,21 +4,21 @@ import java.util.Arrays;
 
 public class SampleBusinessImpl {
 	private DataService dataService;
-	
 
 	public SampleBusinessImpl(DataService dataService) {
 		super();
 		this.dataService = dataService;
 	}
 
-
-
-
 	public int findGreatestFromData() {
 
 		int[] data = dataService.retriveAllData();
 
-		int largevalue = Arrays.stream(data).max().getAsInt();
+		int largevalue = Integer.MIN_VALUE;
+		if (data.length > 0) {
+
+			largevalue = Arrays.stream(data).max().getAsInt();
+		}
 
 		return largevalue;
 	}
